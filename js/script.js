15d1cb5a39d94315f803374bf2cb0408fff98d03
Your Name <you@example.com>
2021-03-08 03:12:44 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// business logic
+function Getpizza(flavour, size, crust, topping, number1) {
+    this.flavour = flavour;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.number1 = number1;
+}
